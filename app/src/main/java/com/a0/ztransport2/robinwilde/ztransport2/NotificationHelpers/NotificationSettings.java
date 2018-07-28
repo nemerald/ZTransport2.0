a9c1b9311c31e1c2cc416948524b388bd7ddd8fd
nemerald <robin_wilde@hotmail.com>
2018-07-28 14:33:26 +0200

    Removed notifications

--- app/src/main/java/com/a0/ztransport2/robinwilde/ztransport2/NotificationHelpers/NotificationSettings.java
+++
@@ -1,8 +0,0 @@
-package com.a0.ztransport2.robinwilde.ztransport2.NotificationHelpers;
-
-public class NotificationSettings {
-    public static String SenderId = "396256928972";
-    public static String HubName = "ZTransportNotificationHub";
-    public static String HubFullAccess = "Endpoint=sb://ztransportnotificationnamespace.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=iBK5URhzBlJ8mi03u/20M5NYlk0UctORWQNDOypHM3Y=";
-    public static String HubListenConnectionString = "Endpoint=sb://ztransportnotificationnamespace.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=wuT8wIWicbzrx23QAbjptD27Q47Xaz1sLaWE1dzVlCo=";
-}
